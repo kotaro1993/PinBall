d788bc961169471781be5dc319c428e7f29dd6ca
kotaro1993 <kotaro.s.1993@gmail.com>
2020-05-22 23:58:50 +0900

    PinBallプロジェクト　初回コミット
    初回コミットです

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreController : MonoBehaviour {
+
+    private GameObject scoreText;
+    public int score;
+    // Use this for initialization
+    void Start () {
+        this.scoreText = GameObject.Find("ScoreText");
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.GetComponent<Text>().text = "Score:" +  score;
+
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+        } else if (collision.gameObject.tag == "LargeStarTag")
+        {
+            score += 20;
+        } else if (collision.gameObject.tag == "SmallCloudTag")
+        {
+            score += 30;
+        } else if (collision.gameObject.tag == "LargeCloudTag")
+        {
+            score += 40;
+        }
+    }
+}
